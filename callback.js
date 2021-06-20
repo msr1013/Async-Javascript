f797f4f7421479329a09139045d0451f73420fbf
Manish Rawal <rawalsinghmanish@gmail.com>
2021-06-20 11:02:53 +0545

    convert-callback-hell-to-promise-using-resolve-reject-then-catch

--- callback.js
+++ callback.js
@@ -1,32 +1,40 @@
 console.log("Before");
-getUser(1, (user) => {
-  console.log("user:", user);
-  getRepositories(user.gitHubUsername, (repos) => {
-    console.log("repos", repos);
-    getCommits("@#$", (commits) => {
-      console.log("commits", commits);
-    });
-  });
-});
+
+getUser(1)
+  .then((user) => getRepositories(user.gitHubUsername))
+  .then((repos) => getCommits(repos[0]))
+  .then((commits) => console.log("commits", commits))
+  .catch((err) => console.log("Error", err.message));
+
 console.log("After");
 
-function getCommits(add, callback) {
-  setTimeout(() => {
-    console.log("Reading a user from a database");
-    callback({ add: add, returnCommit: "returnedcommit" });
-  }, 2000);
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    console.log("getUserCallId", id);
+    //kick off async work
+    setTimeout(() => {
+      console.log("Reading a user from a database");
+      resolve({ id: id, gitHubUsername: "manish" });
+    }, 2000);
+  });
 }
 
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log("Reading a user from a database");
-    callback({ id: id, gitHubUsername: "manish" });
-  }, 2000);
+function getRepositories(username) {
+  console.log("getRepOusernameCall", username);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("reading from repository");
+      resolve(["repo1", "repo2", "repo3"]);
+    }, 2000);
+  });
 }
 
-function getRepositories(username, callback) {
-  setTimeout(() => {
-    console.log("reading from repository");
-    callback(["repo1", "repo2", "repo3"]);
-  }, 2000);
+function getCommits(repo) {
+  console.log("getCommitCall", repo);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("call github api");
+      resolve(["returnedcommit"]);
+    }, 2000);
+  });
 }
